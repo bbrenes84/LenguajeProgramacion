6e4792b96d460acbac7a4bb5e4cbaeb6045b25ab
bbrenes84 <byjimenez2@gmail.com>
2024-02-17 09:56:51 -0600

    Update

---
+++ Sesion 4/Ejemplo_2_Class.py
@@ -0,0 +1,4 @@
+class CuentaBancaria:
+    def __init__(self, saldo_inicial):
+        self.__saldo += cantidad
+        print(f'Se han depositado')
---
+++ Sesion_5/Ejemplo3.py
@@ -0,0 +1,35 @@
+#listas
+lista = [1,2,3,"Manzana",15.6,True]
+print(lista)
+
+#imprimir el primer elemento de la lista
+print(lista[0])
+#tamaño de la lista
+tam= len(lista)
+print(f"el tamaño es {tam}")
+#imprimir el ultimo valor
+print(lista[tam-1])
+
+#imprimir los primero 3 elementos
+print(lista[0:3])
+
+#imprimir los elementos 4 hasta 6
+print(lista[3:7])
+
+#agregar un elemento a la lista
+lista.append("Brenes")
+print(lista)
+
+#imprimir los ultimos 3 elementos de la lista
+final= len(lista)
+inicio= final-3
+print(lista[inicio: final +1])
+
+#Eliminar Manzana
+lista.remove("Manzana")
+lista.remove("Brenes")
+
+
+#ordenar lista
+lista.sort()
+print(lista)
